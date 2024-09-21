c7c58efd45dd90d4d5b9df4185c1ebacf9050767
jessigaitan <jessica.gaitan@colegioinnovar.com>
2024-09-21 12:31:13 -0500

    Add files via upload

---
+++ BD.js
@@ -0,0 +1,78 @@
+let BD = [
+    {
+        nombre: "Acetaminofen 500mg",
+        precio: 1200,
+        categoria: [],
+        stock: 200,
+        imagen: "https://www.disfarma.com.co/fotosproductos/macro_producto_3.jpg"
+    },
+    {
+        nombre: "Loratadina 10mg ",
+        precio: 2500,
+        categoria: [],
+        stock: 600,
+        imagen: "https://www.disfarma.com.co/fotosproductos/macro_producto_3.jpg"
+    }, 
+    {
+        nombre: "Cetirizina 10mg",
+        precio: 2500,
+        categoria: [],
+        stock: 169,
+        imagen: "https://www.disfarma.com.co/fotosproductos/macro_producto_3.jpg"
+    },
+    {
+        nombre: "advil childrem 2g100ml ",
+        precio: 18500,
+        categoria: [],
+        stock: 25,
+        imagen: "https://www.disfarma.com.co/fotosproductos/macro_producto_3.jpg"
+    },
+    {
+        nombre: "ibuprofeno 800mg",
+        precio: 3500,
+        categoria: [],
+        stock: 25,
+        imagen: "https://www.disfarma.com.co/fotosproductos/macro_producto_3.jpg"
+    },
+    {
+        nombre: "",
+        precio: ,
+        categoria: [],
+        stock: 25,
+        imagen: "https://www.disfarma.com.co/fotosproductos/macro_producto_3.jpg"
+    },
+]
+]
+
+let users = [
+    {
+        usename: "admin",
+        pass: "compaq",
+        nombre: "Admin Super",
+        fechaNacimiento: "05/05/1997",
+        document: "2222222",
+        verified: true
+    }
+]
+
+// function login(username="", pass=""){
+
+//     for(let user of users){
+
+//         if(username === user.usename){
+
+//             if(pass === user.pass){
+
+//                 // inicio de sesion exitoso
+//                 return localStorage.setItem('status', 'logged')
+
+//             }
+
+//         }
+//     }
+
+//     return alert('usuario/contraseña incorrectos')
+
+// }
+
+// login("admin", "compaq")
